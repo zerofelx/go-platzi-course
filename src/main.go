9b9ab8e7d3e38ab030019ad81891f444f6f4428e
zerofelx <zeroflex3@gmail.com>
2022-06-14 17:24:38 -0500

    Learning about constants, variables and zero values

--- src/main.go
+++ src/main.go
@@ -3,5 +3,24 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello World")
+	const pi float64 = 3.14
+	const pi2 = 3.1416
+
+	fmt.Println("pi:", pi)
+	fmt.Println("pi2:", pi2)
+
+	// Variables
+	base := 12
+	var altura int = 14
+	var area int
+
+	fmt.Println(base, altura, area)
+
+	// Zero Values
+	var a int
+	var b string
+	var c float64
+	var d bool
+
+	fmt.Println(a, b, c, d)
 }
